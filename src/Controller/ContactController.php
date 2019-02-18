127eede780234ff2a94e3e01982f13d6aa9a9d5f
Valdez <edison.valdez@hp.com>
2019-02-18 15:14:40 +0100

    installed composer require asset

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -6,7 +6,7 @@ use App\Form\ContactType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
- 
+
 
 class ContactController extends AbstractController
 {
