2f41cdf964f9aa1c0af2182a3868c78ee56fd504
Valdez <edison.valdez@hp.com>
2019-02-19 12:21:30 +0100

    Entity & Repository Launch Folders was created

---
+++ src/Entity/LaunchFolders.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\LaunchFoldersRepository")
+ */
+class LaunchFolders
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $code_name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $link;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $folder_description;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $business_unit;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $folder_image;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCodeName(): ?string
+    {
+        return $this->code_name;
+    }
+
+    public function setCodeName(string $code_name): self
+    {
+        $this->code_name = $code_name;
+
+        return $this;
+    }
+
+    public function getLink(): ?string
+    {
+        return $this->link;
+    }
+
+    public function setLink(string $link): self
+    {
+        $this->link = $link;
+
+        return $this;
+    }
+
+    public function getFolderDescription(): ?string
+    {
+        return $this->folder_description;
+    }
+
+    public function setFolderDescription(string $folder_description): self
+    {
+        $this->folder_description = $folder_description;
+
+        return $this;
+    }
+
+    public function getBusinessUnit(): ?string
+    {
+        return $this->business_unit;
+    }
+
+    public function setBusinessUnit(string $business_unit): self
+    {
+        $this->business_unit = $business_unit;
+
+        return $this;
+    }
+
+    public function getFolderImage(): ?string
+    {
+        return $this->folder_image;
+    }
+
+    public function setFolderImage(?string $folder_image): self
+    {
+        $this->folder_image = $folder_image;
+
+        return $this;
+    }
+}
